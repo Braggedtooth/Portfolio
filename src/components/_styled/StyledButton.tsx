42777f7ecde5446f29cf731412f225a2a1c6b02b
Adebayo Ajayi <54339202+Braggedtooth@users.noreply.github.com>
2022-06-13 18:46:46 +0200

    fix(button): fixes button
    remove redundatant props and replaced with style prop

--- src/components/_styled/StyledButton.tsx
+++ src/components/_styled/StyledButton.tsx
@@ -1,47 +1,44 @@
+import React from 'react'
 import styled from 'styled-components'
 import { darken, lighten } from 'polished'
 
 type sizeType = { h: number | string; w: number | string }
 export interface StyledProps {
-  color?: string
-  size: sizeType
+  color: string
+  size: 'small' | 'large'
   borderRadius?: number
-  border?: string
-  bg?: string
-  p?: number
-  m?: number
-  pt?: number
-  pb?: number
-  pl?: number
-  pr?: number
-  mt?: number
-  mb?: number
-  ml?: number
-  mr?: number
+  border: string
+  bg: string
+  p: number | string
+  m: number | string
+  style?: React.CSSProperties
+}
+
+const getSize = (size: 'small' | 'large'): sizeType => {
+  switch (size) {
+    case 'small':
+      return { h: '3rem', w: '3rem' }
+    case 'large':
+      return { h: '3rem', w: '100%' }
+    default:
+      return { h: '2rem', w: '12rem' }
+  }
 }
 
 const StyledButton = styled.button((props: StyledProps) => ({
   color: props.color,
-  height: props.size?.h,
-  width: props.size?.w,
+  height: getSize(props.size).h,
+  width: getSize(props.size).w,
   background: props.bg,
   border: props.border || 'none',
   fontSize: '1.1rem',
   margin: props.m,
   padding: props.p,
-  marginTop: props.mt,
-  marginLeft: props.ml,
-  marginRight: props.mr,
-  marginBottom: props.mb,
-  paddingTop: props.pt,
-  paddingBottom: props.pb,
-  paddingLeft: props.pl,
-  paddingRight: props.pr,
-  borderRadius: props.borderRadius || 25,
+  borderRadius: (props.size === 'small' && '100px') || props.borderRadius,
   '&:hover': {
     backgroundColor: lighten(0.1, props.bg),
     cursor: 'pointer',
-    color: darken(0.5, props.color),
+    color: darken(0.2, props.color),
     transition: '0.2s'
   }
 }))
