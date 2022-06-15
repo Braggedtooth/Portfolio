1cb9b729283ed2982be1b2179de09cd1b3f740db
Adebayo Ajayi <54339202+Braggedtooth@users.noreply.github.com>
2022-06-15 17:34:33 +0200

    feat(projectcard): add project card component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,19 @@
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+import theme from '../config/theme'
+import Layout from '../layout'
+import GlobalStyles from '../config/globalStyle'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Layout>
+          <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
