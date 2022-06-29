459c4fc0d95f7d76952bfa6f80a4d3cb7f00657d
Adebayo Ajayi <54339202+Braggedtooth@users.noreply.github.com>
2022-06-30 00:25:07 +0200

    add components to pages

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,12 +1,11 @@
 import React, { ReactElement } from 'react'
 import { GetStaticProps } from 'next'
-import { lighten } from 'polished'
+import { Box, MediaQuery } from '@mantine/core'
 import { REVALIDATE_SECONDS } from '../utils/constants'
 import { IProject } from '../types/projects'
 import getProjectData from '../data/project'
 import ProjectCard from '../components/atoms/ProjectCard'
 import Layout from '../layout'
-import theme from '../config/theme'
 import getUserData from '../data/user'
 
 type projectProps = {
@@ -14,23 +13,35 @@ type projectProps = {
 }
 const Projects = ({ projects }: projectProps) => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        padding: '0.5rem',
-        margin: '0 0.5rem',
-        width: '100%',
-        flexWrap: 'wrap',
-        justifyContent: 'space-between',
-        borderRadius: '0 0.5rem 0.5rem 0',
-        backgroundColor: lighten(0.1, theme.colors.grey),
-        alignItems: 'flex-start'
+    <MediaQuery
+      largerThan="sm"
+      styles={{
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignContent: 'center',
+        flex: '1 ',
+        flexBasis: '100%',
+        width: '100%'
       }}
     >
-      {projects.map((project) => {
-        return <ProjectCard key={project.id} project={project} />
-      })}
-    </div>
+      <Box
+        sx={(theme) => ({
+          display: 'flex',
+          padding: '1.5rem',
+          width: '100%',
+          height: '100%',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+          borderRadius: '0 0.5rem 0.5rem 0',
+          backgroundColor: theme.colors.grey,
+          alignItems: 'flex-start'
+        })}
+      >
+        {projects.map((project) => {
+          return <ProjectCard key={project.id} project={project} />
+        })}
+      </Box>
+    </MediaQuery>
   )
 }
 
