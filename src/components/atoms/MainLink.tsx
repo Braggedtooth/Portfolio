cdc4060a75856464c9d8d45d715e8291587e26b8
Adebayo Ajayi <54339202+Braggedtooth@users.noreply.github.com>
2022-06-30 00:24:50 +0200

    Redesign components using mantine

---
+++ src/components/atoms/MainLink.tsx
@@ -0,0 +1,54 @@
+import {
+  UnstyledButton,
+  Group,
+  ThemeIcon,
+  Text,
+  MantineColor
+} from '@mantine/core'
+import Link from 'next/link'
+import React from 'react'
+
+interface MainLinkProps {
+  icon: React.ReactNode
+  link: string
+  color: MantineColor
+  label: string
+  close: () => void
+}
+
+function MainLink({ icon, color, label, link , close}: MainLinkProps) {
+  return (
+    <Link passHref href={link}>
+      <UnstyledButton
+        component="a"
+        href={link}
+        sx={(theme) => ({
+          display: 'block',
+          width: '100%',
+          padding: theme.spacing.xs,
+          borderRadius: theme.radius.sm,
+          color:
+            theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
+
+          '&:hover': {
+            backgroundColor:
+              theme.colorScheme === 'dark'
+                ? theme.colors.dark[6]
+                : theme.colors.gray[0]
+          }
+        })}
+        onClick={close}
+      >
+        <Group>
+          <ThemeIcon color={color} variant="light">
+            {icon}
+          </ThemeIcon>
+
+          <Text size="md">{label}</Text>
+        </Group>
+      </UnstyledButton>
+    </Link>
+  )
+}
+
+export default MainLink
--- src/components/molecules/ContactPill.tsx
+++ src/components/molecules/ContactPill.tsx
@@ -1,3 +1,6 @@
+import { Divider, Group, MantineColor, Text, ThemeIcon } from '@mantine/core'
+
+import { darken } from 'polished'
 import React from 'react'
 import { IconType } from 'react-icons'
 import calculateAge from '../../utils/calculateAge'
@@ -6,19 +9,34 @@ import Anchor from '../atoms/Anchor'
 interface ContactPillProps {
   Icon: IconType
   details: string
-  as: 'phone' | 'email' | 'location' | 'birthday'
+  iconColor?: MantineColor
+  textColor?: MantineColor
+  as?: 'phone' | 'email' | 'location' | 'birthday'
 }
-
-const ContactType = ({
-  details,
-  as,
-  children
-}: Omit<ContactPillProps, 'Icon'> & { children: React.ReactElement }) => {
+type ContactPillAnchorProps = {} & Omit<ContactPillProps, 'Icon'>
+const I = ({ details, as }) => {
+  return (
+    <Text size="md">
+      {as === 'birthday'
+        ? `${calculateAge(new Date(details))} years old`
+        : details}
+    </Text>
+  )
+}
+const ContactType = ({ details, as }: ContactPillAnchorProps) => {
   switch (as) {
     case 'phone':
-      return <Anchor href={`tel:${details}`}>{children}</Anchor>
+      return (
+        <Anchor href={`tel:${details}`}>
+          <I details={details} as={as} />
+        </Anchor>
+      )
     case 'email':
-      return <Anchor href={`mailto:${details}`}>{children}</Anchor>
+      return (
+        <Anchor href={`mailto:${details}`}>
+          <I details={details} as={as} />
+        </Anchor>
+      )
     case 'location':
       return (
         <Anchor
@@ -26,25 +44,69 @@ const ContactType = ({
           target="_blank"
           rel="noreferrer"
         >
-          {children}
+          <I details={details} as={as} />
         </Anchor>
       )
     case 'birthday':
-      return <Anchor href="/">{children}</Anchor>
+      return (
+        <Anchor>
+          <I details={details} as={as} />
+        </Anchor>
+      )
     default:
-      return null
+      return <I details={details} as={as} />
   }
 }
-const ContactPill = ({ Icon, details, as }: ContactPillProps) => {
+const ContactPill = ({
+  Icon,
+  details,
+  as,
+  iconColor,
+  textColor
+}: ContactPillProps) => {
+  /*   const Mytheme = useMantineTheme() */
+  /*   const media = useMediaQuery(`(min-width:${Mytheme.breakpoints.md}px)`) */
+
   return (
-    <ContactType details={details} as={as}>
-      <>
-        <Icon size={20} style={{ padding: '0.5rem' }} />
-        {as === 'birthday'
-          ? `${calculateAge(new Date(details))} years old`
-          : details}
-      </>
-    </ContactType>
+    <Group
+      sx={(theme) => ({
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        backgroundColor: theme.colors.gray,
+        color: textColor || theme.colors.dark[0],
+        boxShadow: theme.shadows[3],
+        fontSize: theme.fontSizes.md,
+        justifyContent: 'space-between',
+        textAlign: 'start',
+        alignItems: 'center',
+        padding: theme.spacing.sm,
+        textDecoration: 'none',
+        marginTop: theme.spacing.sm,
+        borderRadius: theme.radius.sm,
+        a: {
+          color: textColor || theme.colors.dark[0],
+          textDecoration: 'none',
+          flex: '1',
+          '&:hover': {
+            textDecoration: 'none',
+            color: darken(0.1, textColor || theme.colors.dark[0])
+          }
+        },
+        '&:hover': {
+          transform: 'scale(1.009)',
+          cursor: 'pointer'
+        }
+      })}
+    >
+      <Group>
+        <ThemeIcon color={iconColor} variant="light">
+          <Icon size={20} />
+        </ThemeIcon>
+        <Divider sx={{ height: 'auto' }} size="md" orientation="vertical" />
+      </Group>
+      <ContactType details={details} as={as} />
+    </Group>
   )
 }
 
