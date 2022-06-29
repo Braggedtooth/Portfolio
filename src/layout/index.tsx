344f3c86524cbe64643e071b043ce3c120418b83
Adebayo Ajayi <54339202+Braggedtooth@users.noreply.github.com>
2022-06-30 00:22:57 +0200

    Implement mantine and config layout

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,7 +1,19 @@
-import React, { useEffect } from 'react'
-import UserData from '../data/user'
-import Header from './Header'
-import Sidebar from './Sidebar'
+import {
+  AppShell,
+  Burger,
+  Text,
+  MediaQuery,
+  Navbar,
+  useMantineTheme,
+  Box,
+  Loader,
+  Center
+} from '@mantine/core'
+import { useRouter } from 'next/router'
+import React, { useEffect, useState } from 'react'
+import { MdContactPage, MdDownload, MdFolder, MdHome } from 'react-icons/md'
+import MainLink from '../components/atoms/MainLink'
+import getUserData from '../data/user'
 
 interface layoutProps {
   data: any
@@ -9,38 +21,126 @@ interface layoutProps {
 }
 
 const Index = ({ data, children }: layoutProps) => {
+  const router = useRouter()
+  const theme = useMantineTheme()
+  const [opened, setOpened] = useState(false)
   const [user, setUser] = React.useState(data.user)
+  const [routesLoading, setRoutesLoading] = React.useState(false)
 
   // Fetch  data on mount if not passed from data prop
   useEffect(() => {
     if (!data.user) {
-      UserData.then((res) => setUser(res))
+      getUserData().then((res) => {
+        setUser(res)
+      })
     }
   }, [data])
 
+  useEffect(() => {
+    const handleStart = (url: string) => {
+      console.log(`Loading: ${url}`)
+      setRoutesLoading(true)
+    }
+    const handleStop = () => {
+      setRoutesLoading(false)
+    }
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleStop)
+    router.events.on('routeChangeError', handleStop)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleStop)
+      router.events.off('routeChangeError', handleStop)
+    }
+  }, [router])
+
   return (
     user && (
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignContent: 'center',
-          width: '100%'
+      <AppShell
+        styles={{
+          main: {
+            background:
+              theme.colorScheme === 'dark'
+                ? theme.colors.dark[4]
+                : theme.colors.gray[0]
+          }
         }}
+        navbarOffsetBreakpoint="sm"
+        asideOffsetBreakpoint="sm"
+        fixed
+        navbar={
+          <Navbar
+            p="md"
+            hiddenBreakpoint="sm"
+            hidden={!opened}
+            width={{ sm: 200, lg: 300 }}
+          >
+            <Navbar.Section grow mt="md">
+              <MainLink
+                icon={<MdHome size={18} />}
+                color="teal"
+                label="Home"
+                link="/"
+                close={() => setOpened(false)}
+              />
+              <MainLink
+                icon={<MdContactPage size={18} />}
+                color="grape"
+                label="Contact"
+                link="/contact"
+                close={() => setOpened(false)}
+              />
+              <MainLink
+                icon={<MdFolder size={18} />}
+                color="indigo"
+                label="Projects"
+                link="/projects"
+                close={() => setOpened(false)}
+              />
+              <MainLink
+                icon={<MdDownload size={18} />}
+                color="orange"
+                label="Resume"
+                link="/resume"
+                close={() => setOpened(false)}
+              />
+            </Navbar.Section>
+            <Navbar.Section>
+              <Box
+                sx={{
+                  paddingTop: theme.spacing.sm,
+                  display: 'flex',
+                  justifyContent: 'center',
+                  borderTop: `1px solid ${
+                    theme.colorScheme === 'dark'
+                      ? theme.colors.dark[4]
+                      : theme.colors.gray[2]
+                  }`
+                }}
+              >
+                <Text size="sm" color="dimmed">
+                  © 2022 bayo.se
+                </Text>
+              </Box>
+            </Navbar.Section>
+          </Navbar>
+        }
+        header={
+          <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
+            <Burger
+              opened={opened}
+              onClick={() => setOpened((o) => !o)}
+              size="md"
+              color={theme.colors.gray[6]}
+              mr="xl"
+            />
+          </MediaQuery>
+        }
       >
-        <Header />
-        <div
-          style={{
-            marginTop: '1rem',
-            display: 'flex',
-            padding: '10rem',
-            justifyContent: 'space-between'
-          }}
-        >
-          <Sidebar user={user} />
-          {children}
-        </div>
-      </div>
+        <Center mb="xl">{routesLoading ? <Loader /> : children}</Center>
+      </AppShell>
     )
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,11 @@
 import type { AppProps } from 'next/app'
-import { ThemeProvider } from 'styled-components'
-import { useRouter } from 'next/router'
 import { NextPage } from 'next'
-import { ReactElement, useEffect } from 'react'
-import NProgress from 'nprogress'
-import theme from '../config/theme'
-import GlobalStyle from '../config/globalStyle'
+import React, { ReactElement, } from 'react'
 import '../../public/nprogress.css'
+import { MantineProvider } from '@mantine/core'
 
 type NextPageWithLayout = NextPage & {
-  getLayout?: (page: ReactElement, _props) => typeof page
+  getLayout?: (page: ReactElement, _props: any) => typeof page
 }
 
 type AppPropsWithLayout = AppProps & {
@@ -19,33 +15,17 @@ type AppPropsWithLayout = AppProps & {
 const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
   const getLayout = Component.getLayout ?? ((page, _props) => page)
 
-  const router = useRouter()
-
-  useEffect(() => {
-    const handleStart = (url) => {
-      console.log(`Loading: ${url}`)
-      NProgress.start()
-    }
-    const handleStop = () => {
-      NProgress.done()
-    }
-
-    router.events.on('routeChangeStart', handleStart)
-    router.events.on('routeChangeComplete', handleStop)
-    router.events.on('routeChangeError', handleStop)
-
-    return () => {
-      router.events.off('routeChangeStart', handleStart)
-      router.events.off('routeChangeComplete', handleStop)
-      router.events.off('routeChangeError', handleStop)
-    }
-  }, [router])
-
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        /** Put your mantine theme override here */
+        colorScheme: 'dark'
+      }}
+    >
       {getLayout(<Component {...pageProps} />, pageProps)}
-    </ThemeProvider>
+    </MantineProvider>
   )
 }
 
