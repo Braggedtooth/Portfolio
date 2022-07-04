85b9afe1e2bd4199f624b568d7272d247ee87aee
Adebayo Ajayi <54339202+Braggedtooth@users.noreply.github.com>
2022-07-04 02:05:11 +0200

    feat(resume): adds reume frame
    uses iframe to have acess to browser api instead of using a package or own implementation
    for viewing and downloading pdf.

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,20 +1,30 @@
-import { Box } from '@mantine/core'
+import { Anchor, Box } from '@mantine/core'
 import React, { ReactElement } from 'react'
+import { MdTab } from 'react-icons/md'
 import Layout from '../layout'
 
 const Resume = () => {
   return (
     <Box
       sx={(theme) => ({
-        display: 'flex',
         padding: '0.5rem',
         margin: '0 0.5rem',
+        height: '90vh',
         width: '100%',
         backgroundColor: theme.colors.grey,
         alignItems: 'flex-start'
       })}
     >
-      resume
+      <iframe
+        src="/resume.pdf"
+        title="Resume"
+        height="100%"
+        width="100%"
+        style={{ border: 'none' }}
+      />
+      <Anchor color="orange" href="resume.pdf" target="_blank">
+        Open in new tab <MdTab size={15} />
+      </Anchor>
     </Box>
   )
 }
